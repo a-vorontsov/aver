16f991bbb57673723ee5f2d4a6a9c2e5a10d4cb9
Aleksandr Vorontsov <aleksandr@vorontsov.co.uk>
2020-11-18 22:15:36 +0000

    Clean up bytecode generation

--- src/rpn-builder/main.ml
+++ src/rpn-builder/main.ml
@@ -16,33 +16,30 @@ let rec gen_expr_bytecode e b =
   | Input -> append_bc INPUT b
   | Int i -> append_bc (LOAD_CONST i) b
   | Var v -> append_bc (LOAD_VAR v) b
-  | Binop (o, x, y) -> (
-      match o with
-      | Add -> gen_expr_bytecode x b @ gen_expr_bytecode y b @ append_bc ADD b
-      | Mult ->
-        gen_expr_bytecode x b @ gen_expr_bytecode y b @ append_bc MULTIPLY b
-      | Div ->
-        gen_expr_bytecode x b @ gen_expr_bytecode y b @ append_bc DIVIDE b
-      | Sub ->
-        gen_expr_bytecode x b @ gen_expr_bytecode y b @ append_bc SUBTRACT b )
+  | Binop (o, x, y) ->
+      gen_expr_bytecode x b @ gen_expr_bytecode y b
+      @ append_bc
+          ( match o with
+          | Add -> ADD
+          | Mult -> MULTIPLY
+          | Div -> DIVIDE
+          | Sub -> SUBTRACT )
+          b
 
 let gen_assign_bytecode a b =
   match a with s, e -> gen_expr_bytecode e b @ append_bc (STORE_VAR s) b
 
 let gen_condition_bytecode c j b =
   match c with
-  | Bincond (o, e, e') -> (
-      match o with
-      | BEquals ->
-        gen_expr_bytecode e b @ gen_expr_bytecode e' b @ append_bc (CMPEQ j) b
-      | BNequals ->
-        gen_expr_bytecode e b @ gen_expr_bytecode e' b
-        @ append_bc (CMPNEQ j) b
-      | GreaterThan ->
-        gen_expr_bytecode e b @ gen_expr_bytecode e' b @ append_bc (CMPLT j) b
-      | LessThan ->
-        gen_expr_bytecode e b @ gen_expr_bytecode e' b @ append_bc (CMPGT j) b
-    )
+  | Bincond (o, e, e') ->
+      gen_expr_bytecode e b @ gen_expr_bytecode e' b
+      @ append_bc
+          ( match o with
+          | BEquals -> CMPEQ j
+          | BNequals -> CMPNEQ j
+          | GreaterThan -> CMPLT j
+          | LessThan -> CMPGT j )
+          b
 
 let rec gen_while_bytecode c sl b =
   let stmts =
@@ -61,7 +58,7 @@ and gen_stmt_bytecode s b =
 let gen_bytecode a =
   List.fold_left (fun acc stmt -> acc @ gen_stmt_bytecode stmt []) [] a
 
-let emit b = List.fold_left (fun acc stmt -> acc ^ as_string stmt ^ "\n") "" b
+let emit b = String.concat "\n" (List.map as_string b)
 
 let parse s =
   let lexbuf = Lexing.from_string s in
@@ -79,12 +76,12 @@ let parse_file f =
   with
   | Lexer.Error msg -> Error (sprintf "%s!" msg)
   | Parser.Error ->
-    Error
-      (sprintf "Syntax error at offset %d:\n%!" (Lexing.lexeme_start filebuf))
+      Error
+        (sprintf "Syntax error at offset %d:\n%!" (Lexing.lexeme_start filebuf))
   | Util.Syntax_error (location, msg) -> (
       match location with
       | Some (line, pos) ->
-        Error (sprintf "Syntax error at %d:%d\n%s" line pos msg)
+          Error (sprintf "Syntax error at %d:%d\n%s" line pos msg)
       | None -> Error (sprintf "%s" msg) )
 
 let write_file s out =
@@ -101,5 +98,5 @@ let () =
     match bc with
     | Ok x -> write_file x (Filename.remove_extension file)
     | Error e ->
-      eprintf "%s" e;
-      exit (-1) )
+        eprintf "%s" e;
+        exit (-1) )
